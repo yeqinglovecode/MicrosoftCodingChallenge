f75ca97533847ccfea71cc91bf7aa46d98808633
yeqinglovecode <jasminehappyeveryday@gmail.com>
2016-10-13 00:30:52 -0700

    Create solution.py

---
+++ otherQuestion/solution.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+def main():
+    num_str = int(raw_input())
+
+    print(num_str)
+
+    for k in xrange(num_str):
+        str1 = raw_input()
+        result = ""
+        length = len(str1)
+
+        i = 0
+
+        while i < length:
+            ch = str1[i]
+            count = 0
+
+            while i < length and str1[i] == ch:
+                count += 1
+                i += 1
+
+            if count > 1:
+                result += str(count)
+
+            result = result + ch
+
+        print(result)
+
+if __name__ == '__main__':
+    main()
